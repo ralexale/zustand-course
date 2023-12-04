70880cb34a7b4c631c00600e6d50c8a4f63ce9d1
ralexale <ralexale@gmail.com>
2023-12-04 12:40:43 -0500

    first commit

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,5 +1,11 @@
-import { IoAccessibilityOutline, IoHeartOutline, IoListOutline, IoLockClosedOutline, IoPawOutline } from 'react-icons/io5';
-import { WhiteCard } from '../../components';
+import {
+  IoAccessibilityOutline,
+  IoHeartOutline,
+  IoListOutline,
+  IoLockClosedOutline,
+  IoPawOutline,
+} from "react-icons/io5";
+import { WhiteCard } from "../../components";
 
 export const Dashboard = () => {
   return (
@@ -9,45 +15,36 @@ export const Dashboard = () => {
       <hr />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-
         <WhiteCard centered>
-          <IoPawOutline size={ 50 } className="text-indigo-600" />
+          <IoPawOutline size={50} className="text-indigo-600" />
           <h2>Osos</h2>
           <p>Información</p>
         </WhiteCard>
 
-
         <WhiteCard centered>
-          <IoAccessibilityOutline size={ 50 } className="text-indigo-600" />
+          <IoAccessibilityOutline size={50} className="text-indigo-600" />
           <h2>Persona</h2>
           <p>Información</p>
         </WhiteCard>
 
-
         <WhiteCard centered>
-          <IoListOutline size={ 50 } className="text-indigo-600" />
+          <IoListOutline size={50} className="text-indigo-600" />
           <h2>Tareas</h2>
           <p>Información</p>
         </WhiteCard>
 
-
         <WhiteCard centered>
-          <IoHeartOutline size={ 50 } className="text-indigo-600" />
+          <IoHeartOutline size={50} className="text-indigo-600" />
           <h2>Boda</h2>
           <p>Información</p>
         </WhiteCard>
 
-
         <WhiteCard centered>
-          <IoLockClosedOutline size={ 50 } className="text-indigo-600" />
+          <IoLockClosedOutline size={50} className="text-indigo-600" />
           <h2>Auth</h2>
           <p>Información</p>
         </WhiteCard>
-
-
-
       </div>
-
     </>
   );
 };
